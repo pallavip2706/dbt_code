df68425be3ba43b8c88417584c5afc519edc7894
Rajesh Mandli <rajm26sept@gmail.com>
2024-09-12 20:55:37 +0000

    commit

---
+++ models/customer.sql
@@ -0,0 +1,7 @@
+{{config(materialized = 'table')}}
+
+with tb1 as (select id,
+            first_name,
+            last_name 
+            from {{source('datafeed_shared_schema','STG_CUSTOMERS')}})
+select * from tb1            
