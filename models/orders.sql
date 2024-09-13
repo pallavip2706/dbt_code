204681892664041204bfa3c4a3f92ef224c6d792
Rajesh Mandli <rajm26sept@gmail.com>
2024-09-13 20:50:32 +0000

    commit

---
+++ models/orders.sql
@@ -0,0 +1,9 @@
+{{config(materialized='view')}}
+
+with tb1 as(
+    select id,
+           user_id,
+           order_date,
+           status from {{source('datafeed_shared_schema','STG_ORDERS')}}
+)
+select * from tb1
